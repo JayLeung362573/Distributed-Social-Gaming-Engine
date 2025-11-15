f7b220a40ebb746ddb2b97dcc5ed16d2e20d3e67
dvc4 <dvc4R@sfu.ca>
2025-11-14 20:31:44 -0800

    Added tests for game runtime, session and entire workflow. Added a constant override for variableMap load function to enable testing

--- tests/GameRuntimeTest.cpp
+++ tests/GameRuntimeTest.cpp
@@ -22,31 +22,3 @@ TEST(GameRuntimeTest, RunExecutesStatementsAndUpdatesState) {
     ASSERT_NE(winnerVal, nullptr); // sanity check
     EXPECT_EQ(winnerVal->asString(), String{"player1"});
 }
-
-// TODO: Revisit once IO implemented in GameInterpreter
-// TEST(GameRuntimeTest, TickCompletesSimpleProgram) {
-//     // Program: winner = "player1"
-//     std::vector<std::unique_ptr<ast::Statement>> stmts;
-//     stmts.push_back(
-//         ast::makeAssignment(
-//             ast::makeVariable(Name{"winner"}),
-//             ast::makeConstant(Value{String{"player1"}})
-//         )
-//     );
-
-//     ast::GameRules rules{std::span(stmts)};
-//     GameRuntime runtime(rules);
-
-//     // No input messages
-//     std::vector<GameMessage> emptyMsgs;
-//     auto outMsgs = runtime.tick(emptyMsgs);
-
-//     EXPECT_TRUE(runtime.isFinished());
-//     EXPECT_TRUE(outMsgs.empty());
-
-//     const VariableMap& state = runtime.getGameState();
-//     Value* winnerVal = state.load(Name{"winner"});
-//     ASSERT_NE(winnerVal, nullptr); // sanity check
-//     EXPECT_EQ(winnerVal->asString(), String{"player1"});
-
-// }

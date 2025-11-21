217583b55d09e89aaf90ae5ad90bcf56cdaef4eb
Jiawei Liang <jla1028@sfu.ca>
2025-11-21 14:34:23 -0800

    add gameSession and gameRuntime

---
+++ src/GameSession/GameSession.cpp
@@ -0,0 +1,54 @@
+#include "GameSession.h"
+#include <iostream>
+#include <utility>
+
+GameSession::GameSession(LobbyID lobbyID, ast::GameRules rules, std::vector<LobbyMember> players)
+    : m_lobbyID(std::move(lobbyID))
+    , m_players(std::move(players))
+    , m_runtime(std::make_unique<GameRuntime>(rules))
+    {
+
+    std::cout << "[GameSession] Created session for lobby " << m_lobbyID
+              << " with " << m_players.size() << " players\n";}
+
+void
+GameSession::start() {
+    if(m_runtime->isFinished()){
+        std::cout << "[GameSession] Cannot start: already finished or inactive\n";
+        return;
+    }
+    // TODO: simulate simple game without player input first
+
+    // TODO: then use tick() with player input
+    m_runtime->run();
+
+    std::cout << "[GameSession] Game for lobby " << m_lobbyID << " finished\n";
+}
+
+// TODO: Revisit once IO implemented in GameInterpreter
+// std::vector<ClientMessage>
+// GameSession::tick(const std::vector<ClientMessage>& incomingMessages){
+//     if(m_runtime->isFinished()){
+//         return {};
+//     }
+
+//     std::vector<GameMessage> gameMsgs = extractGameMessages(incomingMessages);
+
+//     auto outGameMsgs = m_runtime->tick(gameMsgs);
+
+//     if(m_runtime->isFinished()){
+//         return {};
+//     }
+
+//     return convertToClientMessages(outGameMsgs);
+// }
+
+std::vector<GameMessage>
+GameSession::extractGameMessages(const std::vector<ClientMessage> &clientMsgs) {
+    return {};
+}
+
+std::vector<ClientMessage>
+GameSession::convertToClientMessages(const std::vector<GameMessage> &gameMsgs) {
+    return {};
+}
---
+++ src/GameSession/GameSession.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <memory>
+#include "Message.h"
+#include "Lobby/Lobby.h"
+#include "GameEngine/Rules.h"
+#include "GameEngine/GameRuntime.h"
+
+/**
+ * manages a single game instance
+ *
+ * Jobs:
+ * associate a game with a lobby (uses LobbyID)
+ * track game state (finished or not)
+ * track the which players are participating
+ */
+class GameSession{
+public:
+    GameSession(LobbyID lobbyID,
+                ast::GameRules rules,
+                std::vector<LobbyMember> players);
+
+    void start();
+
+    std::vector<ClientMessage> tick(const std::vector<ClientMessage>& incomingMessages);
+
+    bool isFinished() const {return m_runtime->isFinished();}
+    LobbyID getLobbyID() const {return m_lobbyID;}
+    std::vector<ClientMessage> getResultMessages() const;
+
+private:
+    LobbyID m_lobbyID;
+    std::vector<LobbyMember> m_players;
+    std::unique_ptr<GameRuntime> m_runtime;
+
+    std::vector<GameMessage> extractGameMessages(const std::vector<ClientMessage>& clientMsgs);
+    std::vector<ClientMessage> convertToClientMessages(const std::vector<GameMessage>& gameMsgs);
+};

b06d96155ae152e9bf3f996970be48e131838946
Jiawei Liang <jla1028@sfu.ca>
2025-11-27 03:16:38 -0800

    refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "Message.h"
 #include "GameServer.h"
 #include "WebSocketNetworking.h"
 #include "NetworkManager.h"
@@ -9,33 +8,22 @@
 
 
 
-int main(int argc, char* argv[])
-{
-    bool useWebSocket = false;
+int main() {
+    auto networking = std::make_shared<WebSocketNetworking>(8080, "../src/lib/test.html");
 
-    if (argc > 1 && std::string(argv[1]) == "--use-websocket") {
-        useWebSocket = true;
-        std::cout << "Using Web socket network" << "\n";
+    auto server = std::make_shared<GameServer>();
+
+    NetworkManager manager(networking, server);
+
+    networking->startServer();
+
+    while (true) {
+        networking->update();
+
+        manager.processNewConnections();
+
+        manager.processIncomingMessages();
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-
-    if (useWebSocket) {
-        auto networking = std::make_shared<WebSocketNetworking>(8080, "../test.html");
-        auto server = std::make_shared<GameServer>();
-        NetworkManager manager(networking, server);
-
-        networking->startServer();
-
-        auto last = std::chrono::steady_clock::now();
-
-        while (true) {
-            networking->update();
-
-            manager.processNewConnections();
-
-            manager.processIncomingMessages();
-
-            std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        }
-    }
-    return 0;
 }

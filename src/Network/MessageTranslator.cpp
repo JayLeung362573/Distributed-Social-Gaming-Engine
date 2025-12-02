021e4e4715ea96086c553572e001de76c8d92c74
Jiawei Liang <jla1028@sfu.ca>
2025-12-01 16:07:51 -0800

    add colon back on StartGame, update html StartGame button

--- src/Network/MessageTranslator.cpp
+++ src/Network/MessageTranslator.cpp
@@ -22,7 +22,7 @@ struct MessageTraits;
 template<>
 struct MessageTraits<StartGameMessage> {
     // Call statics to enable compile time, so we don't need to instantiate the object and just use it as a container
-    static constexpr std::string_view prefix = "StartGame"; // instantiate it as pure compile time constant
+    static constexpr std::string_view prefix = "StartGame:"; // instantiate it as pure compile time constant
     static std::string serialize(const StartGameMessage& d) {
         return std::string(prefix) + d.playerName;
     }
